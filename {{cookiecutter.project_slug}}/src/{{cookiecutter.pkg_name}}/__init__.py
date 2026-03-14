9b8670cb63be6b233c6ab75dc6f8b67eed0fc8b5
bartosz121 <bmagiera121@gmail.com>
2026-03-14 12:16:52 +0100

    Ignore `RUF067` in init for version

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.pkg_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.pkg_name}}/__init__.py
@@ -1,6 +1,6 @@
 from importlib.metadata import PackageNotFoundError, version
 
-try:
+try:  # noqa: RUF067
     __version__ = version("{{ cookiecutter.project_slug }}")
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
